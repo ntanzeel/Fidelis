ac089172cc2ee7f23797d5266ad687dcb3097f61
ntanzeel <n.tanzeel@hotmail.co.uk>
2016-11-03 15:29:39 +0000

    Renames Category_Tag migration from Pivot to table.

--- database/migrations/2016_11_02_155928_create_category_tag_table.php
+++ database/migrations/2016_11_02_155928_create_category_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoryTagPivot extends Migration
+class CreateCategoryTagTable extends Migration
 {
     /**
      * Run the migrations.
