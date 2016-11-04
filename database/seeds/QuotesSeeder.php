3050aefa8e63db130ba138911e4422f4435066ef
ishegambe <ishegambe@yahoo.co.uk>
2016-11-04 19:13:28 +0000

    Added quotes to QuotesSeeder

--- database/seeds/QuotesSeeder.php
+++ database/seeds/QuotesSeeder.php
@@ -19,5 +19,21 @@ class QuotesSeeder extends Seeder {
             'text'  => 'Love all, trust a few, do wrong to none.',
             'by'    => 'William Shakespeare'
         ]);
+
+        App\Models\Quote::create([
+            'text'  => 'The glue that holds all relationships together--including the relationship between the
+             leader and the led - is trust, and trust is based on integrity.',
+            'by'    => 'Brian Tracy'
+        ]);
+
+        App\Models\Quote::create([
+            'text'  => 'It is mutual trust, even more than mutual interest, that holds human associations together.',
+            'by'    => 'H. L. Mencken'
+        ]);
+
+        App\Models\Quote::create([
+            'text'  => 'Trust is built with consistency.',
+            'by'    => 'Lincoln Chafee'
+        ]);
     }
 }
