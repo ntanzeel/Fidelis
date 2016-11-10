15f53225669f73a7ddb570a5031f2facd4f7e514
TomDaBomb95 <thomasmca123@googlemail.com>
2016-11-10 13:30:36 +0000

    Created Subscriptions migration, seeder and model.

---
+++ database/seeds/SubscriptionsSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SubscriptionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\Models\Subscription::create([
+            'user_id' => 1,
+            'tag_id' => 1
+        ]);
+
+        App\Models\Subscription::create([
+            'user_id' => 1,
+            'tag_id' => 3
+        ]);
+
+        App\Models\Subscription::create([
+            'user_id' => 2,
+            'tag_id' => 1
+        ]);
+
+        App\Models\Subscription::create([
+            'user_id' => 3,
+            'tag_id' => 2
+        ]);
+    }
+}
