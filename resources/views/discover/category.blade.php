9744ac65eca5cb8dd160620f8f8ac28fd161987c
Jordan Olney <jordan_olney@hotmail.co.uk>
2016-11-12 19:23:38 +0000

    Added banner at top of discover page with button

--- resources/views/discover/category.blade.php
+++ resources/views/discover/category.blade.php
@@ -1,17 +1,23 @@
 @extends(config('view.layout', 'layouts.default') . '.app')
 
 @section('content')
+    <div class="discover-header">
+        {{ $category }}
+    </div>
+    <div class="subscribe">
+        <a class="btn-subscribe" href="#">Subscribe</a>
+    </div>
     <div class="col-md-3">
         @include('discover.partials.sidebar', ['categories' => $categories, 'active' => $category])
     </div>
-    <div class="col-md-9">
+    <div class="col-md-6 col-sm-12">
         <div class="panel panel-default">
             <div class="panel-body">
-                <div class="page-title">
-                    {{ $category }}
-                </div>
                 Post
             </div>
         </div>
+    </div>
+    <div class="col-md-3 hidden-sm hidden-xs">
+        @widget('trending')
     </div>
 @stop
--- resources/views/discover/index.blade.php
+++ resources/views/discover/index.blade.php
@@ -1,17 +1,20 @@
 @extends(config('view.layout', 'layouts.default') . '.app')
 
 @section('content')
+    <div class="discover-header">
+        Subscribed
+    </div>
     <div class="col-md-3">
         @include('discover.partials.sidebar', ['categories' => $categories, 'active' => "Subscribed"])
     </div>
-    <div class="col-md-9">
+    <div class="col-md-6 col-sm-12">
         <div class="panel panel-default">
             <div class="panel-body">
-                <div class="page-title">
-                    Subscribed
-                </div>
                 Post
             </div>
         </div>
+    </div>
+    <div class="col-md-3 hidden-sm hidden-xs">
+        @widget('trending')
     </div>
 @stop
