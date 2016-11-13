ba78e80352e8baef12cb7f998e715df50320274b
ntanzeel <n.tanzeel@hotmail.co.uk>
2016-11-13 15:59:19 +0000

    Redesigned posts.

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,15 +1,22 @@
 <div id="post-{{ $post->id }}" class="post">
     <div class="media-left">
         <a href="#">
-            <img class="media-object profile-photo" src="{{ $post->user->photo }}" alt="{{ $post->user->name }} Profile Photo">
+            <img class="media-object avatar author-photo-photo" src="{{ $post->user->photo }}" alt="{{ $post->user->name }} Profile Photo">
         </a>
     </div>
     <div class="media-body">
-        <h4 class="media-heading">
-            <a href="{{ route('profile.view', [$post->user->username]) }}">{{ $post->user->name }}</a>
-        </h4>
-        <div class="post-content">
+        <div class="media-heading post-header info">
+            <a class="author" href="{{ route('profile.view', [$post->user->username]) }}">
+                <strong class="full-name author-name">{{ $post->user->name }}</strong>
+                <span class="username author-username">&commat;{{ $post->user->username }}</span>
+            </a>
+            <span class="time small color light">{{ $post->created_at->diffForHumans() }}</span>
+        </div>
+        <div class="post-body">
             {!! $post->content->htmlText() !!}
+        </div>
+        <div class="post-footer small">
+            X Likes, X Comments
         </div>
     </div>
 </div>
