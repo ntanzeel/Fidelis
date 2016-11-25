ce1653bfcfaea1de7ae8a28bc6fc3b3e47ac1075
ntanzeel <n.tanzeel@hotmail.co.uk>
2016-11-25 13:51:21 +0000

    Double posting fix.

--- public/assets/js/layouts/default/app.js
+++ public/assets/js/layouts/default/app.js
@@ -1,34 +1,4 @@
 $(document).ready(function () {
-    var $posts = $('.post-list').first();
-
-    $('form.ajax').on('submit', function (e) {
-        e.preventDefault();
-
-        var $form = $(this);
-
-        $.ajax({
-            url: $form.attr('action'),
-            method: $form.attr('method'),
-            data: new FormData($form[0]),
-            processData: false,
-            contentType: false,
-            beforeSend: function (xhr) {
-                return xhr.setRequestHeader('X-CSRF-TOKEN', $('meta[name="csrf-token"]').attr('content'));
-            },
-            success: function (response) {
-                $form.trigger('reset');
-                $posts.prepend('<li class="media">' + response + '</li>');
-            },
-            error: function (response) {
-                var errors = response.responseJSON;
-
-                for (var key in errors) {
-                    $form.find('[name="' + key + '"]').parent('.form-group').addClass('has-error');
-                }
-            }
-        });
-    });
-
     /*
      *   Showing posts for notification modal
      */
