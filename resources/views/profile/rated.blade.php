7942d8ff2ef0fd4b464ae787544717c5c763a06f
ntanzeel <n.tanzeel@hotmail.co.uk>
2016-11-28 18:57:06 +0000

    Added content-panel class to enable voting.

--- resources/views/profile/rated.blade.php
+++ resources/views/profile/rated.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-8 col-sm-12">
-            <div class="panel panel-default">
+            <div class="panel panel-default content-panel">
                 <div class="panel-body padding-0">
                     @include('posts.partials.feed', compact('posts'))
                 </div>
