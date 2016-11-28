cb25db7aaa6d1d16b0a8adb34edef1a13685f270
Jordan Olney <jordan_olney@hotmail.co.uk>
2016-11-28 20:07:51 +0000

    Added attributes to follow buttons

---
+++ app/Http/Controllers/Api/FollowersController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordan
+ * Date: 28/11/16
+ * Time: 17:28
+ */
+
+namespace App\Http\Controllers\Api;
+
+
+class FollowersController {
+
+}
---
+++ app/Http/Requests/FollowRequest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordan
+ * Date: 28/11/16
+ * Time: 17:57
+ */
+
+namespace App\Http\Requests;
+
+
+class FollowRequest
+{
+
+}
