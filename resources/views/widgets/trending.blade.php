1513a11d8ed03da397f0c44f2747823c8fb5b56f
ntanzeel <n.tanzeel@hotmail.co.uk>
2016-11-29 15:08:41 +0000

    Tranding widget update and follow button fix

--- resources/views/widgets/trending.blade.php
+++ resources/views/widgets/trending.blade.php
@@ -1,6 +1,10 @@
 <div class="panel panel-default">
     <div class="panel-heading">Trends</div>
     <div class="panel-body">
-        <a href="{{ route("discover.category", 'Brexit') }}">Brexit</a>
+        <ul class="list-unstyled margin-0">
+            <li>
+                <a class="hash-tag" href="{{ route("discover.category", 'Brexit') }}">#Brexit</a>
+            </li>
+        </ul>
     </div>
 </div>
