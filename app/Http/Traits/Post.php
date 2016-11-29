0e383678d44f41219376f73fb88af3ad579f27fe
ntanzeel <n.tanzeel@hotmail.co.uk>
2016-11-29 18:55:25 +0000

    Hashtag regex fix, don't match html entity codes.

--- app/Http/Traits/Post.php
+++ app/Http/Traits/Post.php
@@ -67,7 +67,7 @@ trait Post {
     }
 
     protected function getTags(Models\Comment $comment) {
-        preg_match_all('/#(\w+)/', $comment->text, $tags);
+        preg_match_all('/\B#\w*[a-zA-Z]+\w*/', $comment->text, $tags);
         return empty($tags) ? [] : $tags[1];
     }
 
