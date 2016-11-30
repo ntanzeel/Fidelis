3fffa8d05904d541b3aed7f4fbf7780fbec5d3fb
Ishe Gambe <ishegambe@yahoo.co.uk>
2016-11-30 17:47:00 +0000

    Created lightbox modal to view user images.

---
+++ resources/views/components/ui/lightbox.blade.php
@@ -0,0 +1,10 @@
+<div id="lightbox" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="lightboxmodal" aria-hidden="true">
+    <div class="modal-dialog">
+        <button type="button" class="close hidden" data-dismiss="modal" aria-hidden="true">×</button>
+        <div class="modal-content">
+            <div class="modal-body">
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
+</div>
