534334f71c8f1e4e08faac59a709ad1e1f008a70
Ishe Gambe <ishegambe@yahoo.co.uk>
2016-11-30 13:50:35 +0000

    Modified notifications page to allow users to reply to comments or mentions

--- resources/views/notifications/partials/notification.blade.php
+++ resources/views/notifications/partials/notification.blade.php
@@ -7,37 +7,14 @@
     <div class="media-body">
         <p>{!! $notification->getHtmlText() !!} </p>
 
-        @if(!$notification->isFollow() and !$notification->isVote())
+        @if(!$notification->isFollow())
             <p>{!!   $notification->regarding()->htmlText() !!}</p>
         @else
         @endif
         <div class="notification-footer">
             <ul class="list-inline list-unstyled action-list">
-                @if(!$notification->isFollow() and !$notification->isVote())
-                <li>
-                    <a role="button" class="action action-like"><i class="fa fa-thumbs-up"></i></a>
-                </li>
-                <li>
-                    <a role="button" class="action action-like"><i class="fa fa-thumbs-down"></i></a>
-                </li>
-                {{--<li>--}}
-                    {{--<a role="button" class="action action-comment"--}}
-                       {{--href="{{ route('post.view', [$post->user->username, $post->id]) }}">--}}
-                        {{--<i class="fa fa-comment"></i> {{ $post->comments()->count() }}--}}
-                    {{--</a>--}}
-                {{--</li>--}}
-                <li class="pull-right">
-                    <a role="button" class="action action-flag">
-                        <i class="fa fa-flag"></i>
-                    </a>
-                </li>
-                <li>
-                    {{ $notification->created_at->diffForHumans() }}
-                </li>
-                @else
-                <li>
-                    {{ $notification->created_at->diffForHumans() }}
-                </li>
+                @if(!$notification->isFollow())
+                    @include('posts.partials.actions', ['post' => $notification->regarding()->post,'showComments' => true, 'comment' => $notification->regarding()])
                 @endif
             </ul>
         </div>
