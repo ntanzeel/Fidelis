b37120f1e7d96d70f8b09e2da804c39766ce928e
Ishe Gambe <ishegambe@yahoo.co.uk>
2017-03-01 17:17:29 +0000

    Created settings seeder and modified comments seeder

---
+++ database/seeds/SettingsSeeder.php
@@ -0,0 +1,161 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SettingsSeeder extends DatabaseSeeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        App\Models\Setting::create([
+            'user_id' => 1,
+            'name' => 'Recommendation Preference',
+            'value' => 'Explorer'
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 1,
+            'name' => 'Number of Recommendations',
+            'value' => 10
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 1,
+            'name' => 'Threshold',
+            'value' => 0.6
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 1,
+            'name' => 'Reputation',
+            'value' => 0
+        ]);
+
+        App\Models\Setting::create([
+            'user_id' => 2,
+            'name' => 'Recommendation Preference',
+            'value' => 'Explorer'
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 2,
+            'name' => 'Number of Recommendations',
+            'value' => 10
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 2,
+            'name' => 'Threshold',
+            'value' => 0.6
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 2,
+            'name' => 'Reputation',
+            'value' => 0
+        ]);
+
+        App\Models\Setting::create([
+            'user_id' => 3,
+            'name' => 'Recommendation Preference',
+            'value' => 'Explorer'
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 3,
+            'name' => 'Number of Recommendations',
+            'value' => 10
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 3,
+            'name' => 'Threshold',
+            'value' => 0.6
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 3,
+            'name' => 'Reputation',
+            'value' => 0
+        ]);
+
+        App\Models\Setting::create([
+            'user_id' => 4,
+            'name' => 'Recommendation Preference',
+            'value' => 'Explorer'
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 4,
+            'name' => 'Number of Recommendations',
+            'value' => 10
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 4,
+            'name' => 'Threshold',
+            'value' => 0.6
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 4,
+            'name' => 'Reputation',
+            'value' => 0
+        ]);
+
+        App\Models\Setting::create([
+            'user_id' => 5,
+            'name' => 'Recommendation Preference',
+            'value' => 'Explorer'
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 5,
+            'name' => 'Number of Recommendations',
+            'value' => 10
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 5,
+            'name' => 'Threshold',
+            'value' => 0.6
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 5,
+            'name' => 'Reputation',
+            'value' => 0
+        ]);
+
+        App\Models\Setting::create([
+            'user_id' => 6,
+            'name' => 'Recommendation Preference',
+            'value' => 'Explorer'
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 6,
+            'name' => 'Number of Recommendations',
+            'value' => 10
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 6,
+            'name' => 'Threshold',
+            'value' => 0.6
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 6,
+            'name' => 'Reputation',
+            'value' => 0
+        ]);
+
+        App\Models\Setting::create([
+            'user_id' => 6,
+            'name' => 'Recommendation Preference',
+            'value' => 'Explorer'
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 7,
+            'name' => 'Number of Recommendations',
+            'value' => 10
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 7,
+            'name' => 'Threshold',
+            'value' => 0.6
+        ]);
+        App\Models\Setting::create([
+            'user_id' => 7,
+            'name' => 'Reputation',
+            'value' => 0
+        ]);
+    }
+}
+?>
