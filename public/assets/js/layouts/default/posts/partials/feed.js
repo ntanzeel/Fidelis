26e5d3477017f65a5bb3dd90c49191224c53cf38
ntanzeel <n.tanzeel@hotmail.co.uk>
2017-03-23 14:07:56 +0000

    Image Modal Design.

--- public/assets/js/layouts/default/posts/partials/feed.js
+++ public/assets/js/layouts/default/posts/partials/feed.js
@@ -1,27 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     /** Image modal **/
 
     //If photo is from feed
     $('.media-list').on('click', '.post-image', function (event) {
         event.preventDefault();
+
         var $images = $(this).parent().children();
+        var $modal = $('#image-modal');
+
         if ($images.length == 1) {
             $(".im-arrow").hide();
-        }
-        else {
+        } else {
             $(".im-arrow").show();
         }
 
-        var image = $('#modal-image');
-        image.attr('src', $(this).attr('src'));
-        image.attr('data-post', $(this).parent().parent().attr('id'));
+        var image = $modal.find('.img').first();
+        image.css('background-image', 'url(' + $(this).attr('src') + ')');
 
-        $('#image-modal').modal();
+        $modal.modal();
     });
 
 
-    $('.im-arrow').click(function(event) {
+    $('.im-arrow').click(function (event) {
         event.preventDefault();
         var source = $(this).siblings('#modal-image').attr('src');
         var container = $(this).siblings('#modal-image').attr('data-post')
@@ -30,7 +31,7 @@ $(document).ready(function() {
         if (container != "sidebar") { //Photos are from a post on the feed
             var post = $("#" + container);
             var images = post.children('.post-images').children();
-            index = images.index($('#'+container+' img[src="' + source + '"]'));
+            index = images.index($('#' + container + ' img[src="' + source + '"]'));
 
             if ($(this).hasClass('scroll-left')) {
                 newImage = $(images.get(index - 1)).attr('src');
@@ -45,17 +46,17 @@ $(document).ready(function() {
 
 
             if ($(this).hasClass('scroll-left')) {
-                index = parseInt(index)-1;
+                index = parseInt(index) - 1;
                 if (index < 0) {
                     index = userImgs.length - 1;
                 }
 
-                $(this).siblings('#modal-image').attr('data-index',index);
+                $(this).siblings('#modal-image').attr('data-index', index);
                 newImage = src + '/' + userImgs[index];
             }
             else {
-                index = (parseInt(index)+1) % userImgs.length;
-                $(this).siblings('#modal-image').attr('data-index',index);
+                index = (parseInt(index) + 1) % userImgs.length;
+                $(this).siblings('#modal-image').attr('data-index', index);
                 newImage = src + '/' + userImgs[index];
             }
         }
@@ -66,13 +67,18 @@ $(document).ready(function() {
 
 });
 
-$(document).keydown(function(event) {
+$(document).keydown(function (event) {
     if ($('#image-modal').hasClass('in')) {
         event.preventDefault();
         switch (event.which) {
-            case 37: $('.scroll-left').click(); break;
-            case 39: $('.scroll-right').click(); break;
-            default: return;
+            case 37:
+                $('.scroll-left').click();
+                break;
+            case 39:
+                $('.scroll-right').click();
+                break;
+            default:
+                return;
         }
     }
 });
