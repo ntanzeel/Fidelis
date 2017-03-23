2455efdcd5064abd0e434c910a6270a15ab3810d
Ishe Gambe <ishegambe@yahoo.co.uk>
2017-03-23 12:10:46 +0000

    Fixed user relationship

--- app/Models/Recommendation.php
+++ app/Models/Recommendation.php
@@ -6,15 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recommendation extends Model
 {
-    protected $fillable = [
-        'user_id', 'user_recommendation', 'tag_id', 'response'
-    ];
-
     protected $dates = [
         'deleted_at'
     ];
 
     public function user() {
-        $this->belongsTo('App\Models\User');
+        return $this->belongsTo('App\Models\User');
     }
 }
