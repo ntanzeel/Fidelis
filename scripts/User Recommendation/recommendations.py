d2f7fa49ea453a1e81da4540d3977f38d4147a0c
Ishe Gambe <ishegambe@yahoo.co.uk>
2017-03-28 12:26:46 +0100

    Made changes to recommendations script

--- scripts/User Recommendation/recommendations.py
+++ scripts/User Recommendation/recommendations.py
@@ -7,51 +7,20 @@ from collections import Counter
 
 def get_post_reputation(curs, post_id, min_reputation):
         curs.execute(
-            "SELECT reputation FROM posts WHERE id = " + post_id + ""
+            "SELECT reputation FROM posts WHERE id = {}".format(post_id)
         )
         post_reputation = curs.fetchone()[0]
 
         return post_reputation >= min_reputation
-
-
-# Given the recommendation type, generate default recommendations. For user,
-# recommendations default to users with the highest reputation. The same is done
-# for posts
-def get_default_recommendations(curs, recomendee_id, recommendation_type, min_reputation, num_recommendations):
-    default_recommendations = []
-
-    if recommendation_type == 0:
-        curs.execute(
-            "SELECT PT.tag_id, P.id FROM posts P JOIN post_tag PT ON P.id = "
-            "PT.post_id WHERE P.user_id != " + recomendee_id + ""
-        )
-        posts = curs.fetchall()
-
-        for post in posts:
-            if get_post_reputation(curs, str(post[1]), min_reputation):
-                default_recommendations.append(post)
-    else:
-        curs.execute(
-            "SELECT id, reputation FROM users WHERE reputation >= "
-            + str(min_reputation) + " AND id != " + recomendee_id + " "
-            "ORDER BY reputation DESC"
-        )
-
-        default_recommendations = [(-1, x[0]) for x in curs.fetchall()]
-
-    return default_recommendations[:num_recommendations]
-
 
 # Get the total number of tags being used on Fidelis
 def get_tags(curs):
     curs.execute("SELECT id FROM tags")
     return [x[0] for x in curs.fetchall()]
 
-
 # Generate a count vector that will be used for user similarity
 def generate_count_vector(num_tags):
     return [0] * num_tags
-
 
 # Populate the count vector by counting the number of posts/votes a user has made
 # in each of the tags. Return a vector of tuples, where each tuple is a tag ID-count
@@ -65,8 +34,7 @@ def get_tag_counts(curs, recommendation_type, recomendee_id, num_tags, tags):
             curs.execute(
                 "SELECT T.id, count(P.id), P.created_at FROM tags T JOIN "
                 "post_tag PT ON T.id = PT.tag_id JOIN posts P ON PT.post_id = P.id "
-                "WHERE T.id =" + str(tag) + " and P.user_id =" + recomendee_id + ""
-                " ORDER BY P.created_at"
+                "WHERE T.id = {} and P.user_id = {} ORDER BY P.created_at".format(tag, recomendee_id)
             )
             counts = curs.fetchall()
         # User recommendation
@@ -75,8 +43,8 @@ def get_tag_counts(curs, recommendation_type, recomendee_id, num_tags, tags):
                 "SELECT T.id, count(V.id), V.created_at FROM tags T JOIN "
                 "post_tag PT ON T.id = PT.tag_id JOIN posts P on PT.post_id = P.id "
                 "JOIN comments C on P.id = C.post_id JOIN votes V on C.id = V.comment_id "
-                "WHERE T.id = " + str(tag) + " AND V.user_id = " + recomendee_id + " "
-                "AND V.type = 1 ORDER BY V.created_at"
+                "WHERE T.id = {} AND V.user_id = {} AND V.type = 1 ORDER BY "
+                "V.created_at".format(tag, recomendee_id)
             )
             counts = curs.fetchall()
 
@@ -86,6 +54,33 @@ def get_tag_counts(curs, recommendation_type, recomendee_id, num_tags, tags):
     return recomendee_vector
 
 
+# Given the recommendation type, generate default recommendations. For user,
+# recommendations default to users with the highest reputation. The same is done
+# for posts
+def get_default_recommendations(curs, recomendee_id, recommendation_type, min_reputation, num_recommendations):
+    default_recommendations = []
+
+    if recommendation_type == 0:
+        curs.execute(
+            "SELECT PT.tag_id, P.id FROM posts P JOIN post_tag PT ON P.id = "
+            "PT.post_id WHERE P.user_id != {}".format(recomendee_id)
+        )
+        posts = curs.fetchall()
+
+        for post in posts:
+            if get_post_reputation(curs, str(post[1]), min_reputation):
+                default_recommendations.append(post)
+    else:
+        curs.execute(
+            "SELECT id, reputation FROM users WHERE reputation >= {} AND id != "
+            "{} ORDER BY reputation DESC".format(min_reputation, recomendee_id)
+        )
+
+        default_recommendations = [(-1, x[0]) for x in curs.fetchall()]
+
+    return default_recommendations[:num_recommendations]
+
+
 # Generate friend-of-a-friend recommendations by looking at all the users followed
 # by the followees of the recomendee. Use these users to either provide user recommendations,
 # or get the content they post to provide content recommendation
@@ -93,8 +88,7 @@ def get_fof_recommendations(curs, recommendation_type, recomendee_id, num_recomm
     fof_recommendations = []
 
     curs.execute(
-        "SELECT following_id FROM followers WHERE follower_id = " + recomendee_id + " "
-        "AND following_id != " + recomendee_id + ""
+        "SELECT following_id FROM followers WHERE follower_id = {}".format(recomendee_id)
     )
     recomendee_followees = [x[0] for x in curs.fetchall()]
 
@@ -104,12 +98,12 @@ def get_fof_recommendations(curs, recommendation_type, recomendee_id, num_recomm
             "SELECT following_id FROM followers WHERE follower_id = " + str(f) + " "
             "AND following_id != " + recomendee_id + ""
         )
-        fofs = [x[0] for x in curs.fetchall()]
+        fofs = list(set([x[0] for x in curs.fetchall()]))
 
         if not set(fofs).issubset(recomendee_followees):
             # Append recommendations as (Tag ID, recommendation). FOF doesn't user_id
             # tags to generate recommendtions so set ID to -1
-            fof_recommendations.append([x[0] for x in fofs])
+            fof_recommendations.append([x for x in fofs])
 
     # Flatten list of recommendations
     fof_recommendations = [val for sublist in fof_recommendations for val in sublist]
@@ -122,7 +116,7 @@ def get_fof_recommendations(curs, recommendation_type, recomendee_id, num_recomm
             curs.execute(
                 "SELECT T.id, P.id FROM posts P JOIN post_tag PT ON P.id = "
                 "PT.post_id JOIN tags T ON PT.tag_id = T.id WHERE P.user_id = "
-                + str(user) + ""
+                "{}".format(user)
             )
             posts = curs.fetchall()
 
@@ -165,8 +159,8 @@ def get_explorer_recommendations(curs, recommendation_type, recomendee_id, recom
         tag_name = curs.fetchone()[0]
         curs.execute(
             "SELECT DISTINCT P.user_id FROM posts P JOIN post_tag PT ON "
-            "P.id = PT.post_id JOIN tags T ON PT.tag_id = T.id WHERE T.id = "
-            + str(tag) + " AND P.user_id != " + recomendee_id + ""
+            "P.id = PT.post_id JOIN tags T ON PT.tag_id = T.id WHERE T.id = {} "
+            "AND P.user_id != {}".format(tag, recomendee_id)
         )
         users = [x[0] for x in curs.fetchall()]
 
@@ -175,16 +169,11 @@ def get_explorer_recommendations(curs, recommendation_type, recomendee_id, recom
             if recommendation_type == 0:
                 curs.execute(
                     "SELECT P.id FROM tags T join post_tag PT ON T.id = PT.tag_id "
-                    "JOIN posts P on PT.post_id = P.id WHERE P.user_id = "
-                    + str(u) + " AND T.id = " + str(tag) + " "
+                    "JOIN posts P on PT.post_id = P.id WHERE P.user_id = {} AND "
+                    "T.id = {}".format(u, tag)
                 )
 
                 posts = [x[0] for x in curs.fetchall()]
-                curs.execute("SELECT name FROM users WHERE id = " + str(u) + "")
-                name = curs.fetchone()[0]
-
-                curs.execute("SELECT text FROM tags WHERE id = " + str(tag) + "")
-                t_name = curs.fetchone()[0]
 
                 for post in posts:
                     if get_post_reputation(curs, str(post), min_reputation):
@@ -227,30 +216,33 @@ def get_hybrid_recommendations(curs, recommendation_type, recomendee_id, recomen
 def get_followees(curs, recomendee_id):
     curs.execute(
         "SELECT following_id FROM followers WHERE "
-        "follower_id = "+ recomendee_id + ""
+        "follower_id = {}".format(recomendee_id)
     )
     return [(-1, x[0]) for x in curs.fetchall()]
 
 
-# Get resolved (accepted/rejected) recommendations for a specific user
-def get_resolved_recommendations(curs, recommendation_type, recomendee_id):
+# Get recommendations that have already been generated for a given user
+def get_current_recommendations(curs, recommendation_type, recomendee_id):
     if recommendation_type == 0:
         curs.execute(
             "SELECT tag_id, content_recommendation FROM content_recommendations WHERE "
-            "user_id = " + recomendee_id + " AND (response = 1 OR response = -1)"
+            "user_id = {}".format(recomendee_id)
         )
+        current_recommendations = curs.fetchall()
     else:
         curs.execute(
-            "SELECT tag_id, user_recommendation FROM user_recommendations WHERE user_id "
-            "= " + recomendee_id + " AND (response = 1 OR response = -1)"
+            "SELECT user_recommendation FROM user_recommendations WHERE user_id "
+            "= {}".format(recomendee_id)
         )
-    return curs.fetchall()
+        current_recommendations = [(-1, x[0]) for x in curs.fetchall()]
+
+    return current_recommendations
 
 
 # Get blocked users for a specific user
 def get_blocked_users(curs, recomendee_id):
     curs.execute(
-        "SELECT blocked_id FROM blocked WHERE blocker_id=" + recomendee_id + ""
+        "SELECT blocked_id FROM blocked WHERE blocker_id = {}".format(recomendee_id)
     )
     return [(-1, x[0]) for x in curs.fetchall()]
 
@@ -281,7 +273,7 @@ def generate_recommendations(curs, recomendee_id, recommendation_type, preferenc
     # Trim recommendation list by removing accepted/rejected recommendations, along with blocked users
     # and the recomendees followees
     recommendations = list(set(recommendations) -
-    set(get_resolved_recommendations(curs, recommendation_type, recomendee_id))
+    set(get_current_recommendations(curs, recommendation_type, recomendee_id))
     - set(get_blocked_users(curs, recomendee_id))
     - set(get_followees(curs, recomendee_id)) - set(recomendee_id))
 
@@ -309,19 +301,8 @@ def generate_recommendations(curs, recomendee_id, recommendation_type, preferenc
                 "INSERT INTO user_recommendations VALUES (Null, %s, %s, %s, %s, DEFAULT, DEFAULT, DEFAULT)",
                 (recomendee_id, r[1], r[0], 0)
             )
-    conn.commit()
 
-
-# Establish database connection
-conn = mysql.connect(user='root', password='', host='localhost', database='fidelis')
-curs = conn.cursor()
-
-curs.execute("SELECT id FROM users")
-fidelis_users = [x[0] for x in curs.fetchall()]
-
-for user in fidelis_users:
-    user = str(user)
-
+def get_user_settings(curs, user):
     curs.execute(
         "SELECT settings.id AS id, (CASE WHEN settings.user_id IS NULL THEN {} "
         "ELSE settings.user_id END) AS user_id, default_settings.name AS name, "
@@ -331,42 +312,64 @@ for user in fidelis_users:
         "settings.deleted_at IS NULL) settings ON default_settings.name = "
         "settings.name WHERE default_settings.deleted_at IS NULL".format(user, user)
     )
-    settings = curs.fetchall()
+    return curs.fetchall()
 
-    # Get all relevant user settings
-    for setting in settings:
-        if setting[2] == 'recommendation_preference':
-            preference = str(setting[3])
-        elif setting[2] == 'recommendation_number':
-            num_recommendations = int(setting[3])
-        elif setting[2] == 'recommendation_threshold':
-            similarity_threshold = float(setting[3])
-        elif setting[2] == 'recommendation_reputation':
-            min_reputation = int(setting[3])
+# Get the number of generated recommendations the user has not responsed to in the
+# specified table
+def get_recommendation_count(curs, user, table):
 
     curs.execute(
-        "SELECT count(id) FROM content_recommendations WHERE user_id=" + user + " "
-        "AND response=0"
+        "SELECT count(id) FROM {} WHERE user_id = {} AND response = 0".format(table, user)
     )
-    remaining_content = curs.fetchone()[0]
+    return curs.fetchone()[0]
 
-    curs.execute(
-        "SELECT count(id) FROM user_recommendations WHERE user_id=" + user + " "
-        "AND response=0"
-    )
-    remaining_users = curs.fetchone()[0]
+def main():
+    # Establish database connection
+    print "Establishing database connection..."
+    conn = mysql.connect(user='root', password='', host='localhost', database='fidelis')
 
-    curs.execute("SELECT name FROM users WHERE id="+user+"")
-    name = curs.fetchone()[0]
+    if conn.is_connected():
+        curs = conn.cursor()
 
-    if remaining_content < num_recommendations:
-        generate_recommendations(curs, user, 0, preference, num_recommendations - remaining_content,
-        similarity_threshold, min_reputation)
+        curs.execute("SELECT id FROM users")
+        fidelis_users = [x[0] for x in curs.fetchall()]
 
-    if remaining_users < num_recommendations:
-        generate_recommendations(curs, user, 1, preference, num_recommendations - remaining_users,
-        similarity_threshold, min_reputation)
+        for user in fidelis_users:
+            settings = get_user_settings(curs, str(user))
 
-# Close connection to database
-curs.close()
-conn.close()
+            # Get all relevant user settings
+            for setting in settings:
+                if setting[2] == 'recommendation_preference':
+                    preference = str(setting[3])
+                elif setting[2] == 'recommendation_number':
+                    num_recommendations = int(setting[3])
+                elif setting[2] == 'recommendation_threshold':
+                    similarity_threshold = float(setting[3])
+                elif setting[2] == 'recommendation_reputation':
+                    min_reputation = int(setting[3])
+
+            content_count = get_recommendation_count(curs, str(user), 'content_recommendations')
+            user_count = get_recommendation_count(curs, str(user), 'user_recommendations')
+
+            curs.execute("SELECT name FROM users WHERE id = {}".format(str(user)))
+            name = curs.fetchone()[0]
+
+            if content_count < num_recommendations:
+                generate_recommendations(curs, str(user), 0, preference, num_recommendations - content_count,
+                similarity_threshold, min_reputation)
+                conn.commit()
+
+            if user_count < num_recommendations:
+                generate_recommendations(curs, str(user), 1, preference, num_recommendations - user_count,
+                similarity_threshold, min_reputation)
+                conn.commit()
+
+        print "Finished recommendation generation...Closing database connection..."
+        # Close connection to database
+        curs.close()
+        conn.close()
+    else:
+        print "Sorry, there was an error connecting to the database"
+
+if __name__ == '__main__':
+    main()
