520494ad6eb32170a16c79b207b610009b5aead7
Jordan Olney <jordan_olney@hotmail.co.uk>
2017-04-07 00:20:37 +0100

    Trained SGD categorisation model

--- scripts/Categorisation/train.py
+++ scripts/Categorisation/train.py
@@ -1,22 +1,33 @@
-import preprocess as pre
-import pandas as pd
+# Data Structures
 import numpy as np
+import pandas as pd
+
+# Feature Engineering
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfTransformer
+
+# Models
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+
+# Pipeline
+from sklearn.pipeline import Pipeline
+
+# Exporting
 from sklearn.externals import joblib
 
-if __name__ == "__main__":
-    dataframe = pd.read_csv('dataset.csv', sep=',', header=None)
-    text = dataframe[0]
-    category = dataframe[1]
-    vectorizer = CountVectorizer(preprocessor=pre.preprocess)
-    X = vectorizer.fit_transform(text)
-    df = pd.DataFrame(data=X)
+if __name__ == '__main__':
+    data = pd.read_csv('data/dataset.csv', header=None, names=['Post', 'Category']) # Load dataframe
+    data['Category'], categories = pd.factorize(data.Category) # Replace text categories with integers
+    data = data[data.Post.str.len() > 20].reset_index(drop=True) # Drop samples with text less than 20 characters long
 
-    # naiveBayes = GaussianNB()
-    # model = naiveBayes.fit(X, category)
-    # joblib.dump(model, 'models/NaiveBayes.pkl')
+    # Pipeline to create the model and create features
+    pipeline = Pipeline([('vect', CountVectorizer(ngram_range=(1, 2))), ('tfidf', TfidfTransformer(use_idf=True)), ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=0.0001, n_iter=10, random_state=42))])
 
-    print(df)
+    # Train the model on the data
+    model = pipeline.fit(data.Post, data.Category.values)
+
+    # Export model
+    joblib.dump(model,'models/SGD.pkl')
