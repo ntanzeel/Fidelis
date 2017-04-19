dc744c5e0d4751d99e2aecfd7ca3b903c50945aa
TomDaBomb95 <thomasmca123@googlemail.com>
2017-04-19 07:45:14 +0100

    Fixed issue with deleted_at for reports in user rep scoring

--- scripts/Reputation Scoring/User_Rep_Score.py
+++ scripts/Reputation Scoring/User_Rep_Score.py
@@ -11,7 +11,7 @@ cursor = cnx.cursor(buffered=True)
 
 #Get each user's total positive vote rep, negative vote rep and comment rep.
 #Returns table in the form: user_id, positive, negative, comment.
-query = ("SELECT t1.*, t2.comments, t3.followers, t4.reports FROM (SELECT user_id, SUM(CASE WHEN root = 1 THEN 0.3 ELSE 0.1 END * up_votes) AS positive, SUM(CASE WHEN root = 1 THEN 0.3 ELSE 0.1 END * down_votes) AS negative FROM comments GROUP BY user_id) t1 LEFT JOIN (SELECT p.user_id, COUNT(c.id) * 0.2 comments FROM comments c LEFT JOIN posts p ON c.post_id = p.id WHERE c.user_id != p.user_id GROUP BY p.user_id) t2 ON t1.user_id = t2.user_id LEFT JOIN (SELECT followers.following_id AS user_id, COUNT(DISTINCT followers.follower_id) * 0.5 AS followers FROM followers INNER JOIN (SELECT * FROM followers WHERE approved = 1 ) subq ON followers.follower_id = subq.following_id GROUP BY followers.following_id) t3 ON t1.user_id = t3.user_id LEFT JOIN (SELECT c.user_id AS user_id, COUNT(*) * 0.5 AS reports FROM reports r LEFT JOIN comments c ON c.id = r.comment_id GROUP BY c.user_id) t4 ON t1.user_id = t4.user_id")
+query = ("SELECT t1.*, t2.comments, t3.followers, t4.reports FROM (SELECT user_id, SUM(CASE WHEN root = 1 THEN 0.3 ELSE 0.1 END * up_votes) AS positive, SUM(CASE WHEN root = 1 THEN 0.3 ELSE 0.1 END * down_votes) AS negative FROM comments GROUP BY user_id) t1 LEFT JOIN (SELECT p.user_id, COUNT(c.id) * 0.2 comments FROM comments c LEFT JOIN posts p ON c.post_id = p.id WHERE c.user_id != p.user_id GROUP BY p.user_id) t2 ON t1.user_id = t2.user_id LEFT JOIN (SELECT followers.following_id AS user_id, COUNT(DISTINCT followers.follower_id) * 0.5 AS followers FROM followers INNER JOIN (SELECT * FROM followers WHERE approved = 1 ) subq ON followers.follower_id = subq.following_id GROUP BY followers.following_id) t3 ON t1.user_id = t3.user_id LEFT JOIN (SELECT c.user_id AS user_id, COUNT(*) * 0.5 AS reports FROM reports r LEFT JOIN comments c ON c.id = r.comment_id WHERE r.deleted_at IS NULL GROUP BY c.user_id) t4 ON t1.user_id = t4.user_id")
 cursor.execute(query)
 
 d = pd.DataFrame(np.zeros((cursor.rowcount, 2)))
@@ -37,6 +37,8 @@ for user_id, positive, negative, comments, followers, reports in cursor:
     d.set_value(count, 1, reputation)
 
     count += 1
+
+print(d)
 
 #get min and max recorded reputation scores, then find difference between them
 min_rep = d[1].min()
